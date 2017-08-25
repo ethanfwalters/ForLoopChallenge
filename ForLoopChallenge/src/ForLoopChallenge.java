6b2c962f86f308dd3824e9c295978c7b6d1c08a7
ethanwalters <ethanwalters@192.168.8.94>
2017-08-25 09:09:05 -0600

    added challenge five

--- ForLoopChallenge/src/ForLoopChallenge.java
+++ ForLoopChallenge/src/ForLoopChallenge.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class ForLoopChallenge
 	{
@@ -36,17 +37,48 @@ public class ForLoopChallenge
 
 				// challenge four
 
+				int bigValue = 0;
+
 				for (int i = 0; i < 105; i = i + 5)
 					{
-						int value = i;
-						int bigValue = i + value;
-						
-						if(i == 100){
-						
-						System.out.println(bigValue);
-						
-						}
+
+						bigValue = bigValue + i;
+
+						if (i == 100)
+							{
+
+								System.out.println(bigValue);
+
+							}
 					}
+
+				// challenge five
+				Scanner userInput = new Scanner(System.in);
+
+				System.out.println("Enter a lowerbound number:");
+
+				int lowerBound = userInput.nextInt();
+
+				System.out.println("Now enter a upperBound");
+
+				int upperBound = userInput.nextInt();
+
+				int outPutValue = 0;
+
+				for (int i = lowerBound; i <= upperBound; i++)
+					{
+
+						outPutValue = outPutValue + i;
+
+						if (i == upperBound)
+							{
+								System.out.println("The sum for all the values " + lowerBound + " and " + upperBound
+										+ " is " + outPutValue);
+							}
+
+					}
+
+				// challenge six
 
 			}
 
