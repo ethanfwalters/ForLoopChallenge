d66bdf90fe41851251bd514184ab2ce10190e323
ethanwalters <ethanwalters@192.168.8.94>
2017-08-25 07:59:17 -0600

    initial commit

---
+++ ForLoopChallenge/src/ForLoopChallenge.java
@@ -0,0 +1,53 @@
+
+public class ForLoopChallenge
+	{
+
+		public static void main(String[] args)
+			{
+				// challenge one
+
+				for (int i = 0; i < 5; i++)
+					{
+						System.out.println("I love Doughnuts!");
+					}
+				;
+
+				// challenge two
+
+				for (int i = 0; i < 5; i++)
+					{
+						int value = i + 1;
+						System.out.println(value);
+					}
+
+				for (int i = 0; i < 5; i++)
+					{
+						int value = 5 - i;
+						System.out.println(value);
+					}
+
+				// challenge three
+
+				for (int i = 0; i < 105; i = i + 5)
+					{
+						System.out.println(i);
+					}
+				;
+
+				// challenge four
+
+				for (int i = 0; i < 105; i = i + 5)
+					{
+						int value = i;
+						int bigValue = i + value;
+						
+						if(i == 100){
+						
+						System.out.println(bigValue);
+						
+						}
+					}
+
+			}
+
+	}
